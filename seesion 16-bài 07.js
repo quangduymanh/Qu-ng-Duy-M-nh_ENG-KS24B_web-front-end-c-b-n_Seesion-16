44fdc3314af8ad016a1e9d041804264b318ef6dd
quangduymanh <quangduymanh4@gmail.com>
2025-02-26 23:27:10 +0700

    upload bài 07

---
+++ seesion 16-bài 07.js
@@ -0,0 +1,5 @@
+let math = +prompt ("nhập điểm môn toán");
+let physics = +prompt ("nhập điểm môn vật lý");
+let chemistry = +prompt ("nhập điểm môn hóa");
+let sum= math + physics + chemistry;
+document.write(sum);
