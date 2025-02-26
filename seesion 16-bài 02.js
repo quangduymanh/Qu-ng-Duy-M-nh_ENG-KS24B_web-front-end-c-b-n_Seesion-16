d3c103826cf82e81aa1e75adfc89c35b5d3b5788
quangduymanh <quangduymanh4@gmail.com>
2025-02-26 23:23:11 +0700

    upload bài 02

---
+++ seesion 16-bài 02.js
@@ -0,0 +1,8 @@
+let number=10;
+document.write(`<p>${number}</p>`);
+let price=20.5;
+document.write(`<p>${price}</p>`);
+let status = ("true");
+document.write(`<p>${status}</p>`);
+let city= ("Hà Nội");
+document.write(`<p>${city}</p>`);
